bc1b1e442b906a8c51a1c6ce03270df8db9a860a
fedoseevm <maksim.fedoseev122@gmail.com>
2025-09-16 19:54:08 +0200

    Complete tasks

---
+++ 01. Algorytm zachlanny - wydawanie reszty/zadanie4.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+romanianNumbers = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+
+
+number = randint(1, 3999)
+print(number, end=" = ")
+for value in romanianNumbers:
+    while number >= value:
+        number -= value
+        print(romanianNumbers[value], end="")
