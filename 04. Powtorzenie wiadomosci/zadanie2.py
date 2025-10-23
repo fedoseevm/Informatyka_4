8028886203dfbbd240dc4dd221e4c83e51fbd3c5
fedoseevm <maksim.fedoseev122@gmail.com>
2025-10-23 19:04:43 +0200

    Complete zadanie2

--- 04. Powtorzenie wiadomosci/zadanie2.py
+++ 04. Powtorzenie wiadomosci/zadanie2.py
@@ -4,9 +4,25 @@ with open('macierz.txt', 'r') as file:
         matrix.append(list(map(int, line.split())))
 print(*matrix, sep="\n")
 
-
 sum = matrix[0][0]
 path = ""
 i = 0
 j = 0
 
+while i != len(matrix) - 1 or j != len(matrix[0]) - 1:
+    if i == len(matrix) - 1:
+        j += 1
+        path += "R"
+    elif j == len(matrix[0]) - 1:
+        i += 1
+        path += "D"
+    elif matrix[i + 1][j] >= matrix[i][j + 1]:
+        i += 1
+        path += "D"
+    else:
+        j += 1
+        path += "R"
+    sum += matrix[i][j]
+
+print("Suma:", sum)
+print("Sciezka:", path)
